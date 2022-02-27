bf3850364067ac83abd53e3696ddadce8e1bb051
Jan Librowski <janlibrowski@gmail.com>
2022-02-28 01:43:34 +0200

    Make the bar reserve space in the bottom

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -4,15 +4,51 @@
 )]
 
 use tauri::Manager;
+use gtk::prelude::*;
 
 fn main() {
   tauri::Builder::default()
     .setup(|app| {
         let main_window = app.get_window("main").unwrap();
+        let gdk_window = main_window.gtk_window().unwrap().window().unwrap();
+        gdk_window.hide();
 
-            println!("---");
-            print!("{:?}", main_window.gtk_window().unwrap());
-            println!("---");
+        gdk_window.set_type_hint(gtk::gdk::WindowTypeHint::Dock);
+
+        let monitor = main_window.primary_monitor().unwrap().unwrap();
+        let size = monitor.size();
+        let position = monitor.position();
+
+
+        let bar_width = size.width as u64;
+        let bar_height: u32 = 72;
+
+        let bar_x = position.x;
+        let bar_y = (position.y as i32) + (size.height as i32) - 72;
+
+        gdk_window.move_resize(bar_x, bar_y, bar_width as i32, bar_height as i32);
+
+        gtk::gdk::property_change(
+            &gdk_window,
+            &gtk::gdk::Atom::intern("_NET_WM_STRUT"),
+            &gtk::gdk::Atom::intern("CARDINAL"),
+            32,
+            gtk::gdk::PropMode::Replace,
+            gtk::gdk::ChangeData::ULongs(&[0, 0, 0, bar_height as u64])
+        );
+
+        gdk_window.show();
+
+        /* gtk::gdk::property_change(
+            &gdk_window,
+            &gtk::gdk::Atom::intern("_NET_WM_STRUT_PARTIAL"),
+            &gtk::gdk::Atom::intern("CARDINAL"),
+            32,
+            gtk::gdk::PropMode::Replace,
+            gtk::gdk::ChangeData::ULongs(&[0, 0, bar_height as u64, 0, 0, 0, 0, 0, position.x as u64, (position.x as u64) + bar_width - 1, 0, 0])
+        ); */
+
+        gdk_window.stick();
 
         Ok(())
     })
--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import "./App.css";
-import "./config";
 
 function App() {
   return (
